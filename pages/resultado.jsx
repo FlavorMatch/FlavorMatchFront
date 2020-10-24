9b3ee7f51c6cfded23fe4c0d6ecc41fef613b7eb
luiza sanchotene <luli.sanchotene@gmail.com>
2020-10-23 23:20:24 -0300

    feat: cria pages e implementa react navigation

---
+++ pages/resultado.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, Button, View, SafeAreaView, TextInput, Alert } from 'react-native';
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+export default function Resultado() {
+  const [value, onChangeText] = React.useState('');
+  const handleClick = () => {
+    
+  }
+    return (
+  <SafeAreaView style={styles.container}>
+    <View>
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+      placeholder='Mudou:'
+      />
+      <Button
+        title="Press me"
+        color="#f194ff"
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+      />
+    </View>
+    <Separator />
+
+  </SafeAreaView>
+)};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
