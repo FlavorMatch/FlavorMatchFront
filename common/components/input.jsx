1528b6ef3580de88a1ec123e25f4314e2cb89eaa
Heitor Maciel <heitor.maciell@gmail.com>
2020-11-16 23:50:00 -0300

    feat: cria componente input

---
+++ common/components/input.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+
+const Input = ({onChangeText, value, placeholder, placeholderTextColor}) => {
+    return (
+    <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={value}
+        placeholder={placeholder}
+        placeholderTextColor={placeholderTextColor} 
+    />
+)};
+
+export default Input;
+
+const styles = StyleSheet.create({
+    input: {
+      height: 50, 
+      marginBottom: '10px',
+      marginHorizontal: '20px',
+      paddingLeft: '10px',
+      boxShadow: '0px 2px 2px #A99CC8',
+      borderRadius: '10px',
+      backgroundColor: '#FFBD59',
+      fontSize: 16,
+      fontFamily: "Purisa",
+      color: '#AF660D',
+    },
+});
