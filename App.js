6a2535611fedd7eeb9a1082ddb1ce7ae47f1c4fb
Heitor Maciel <heitor.maciell@gmail.com>
2020-11-28 16:11:11 -0300

    feat: insere transiçao entre telas e remove header padrao react navigation

--- App.js
+++ App.js
@@ -3,19 +3,61 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Resultado from './pages/resultado';
 import HomeScreen from './pages';
-import Scaffold from './pages/scaffold';
 import Opcoes from './pages/opcoes';
 
 const Stack = createStackNavigator();
 
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Scaffold">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Resultado" component={Resultado} />
-        <Stack.Screen name="Opcoes" component={Opcoes} />
-        <Stack.Screen name="Scaffold" component={Scaffold} />
+      <Stack.Navigator   
+        screenOptions={{
+          headerShown: false
+        }} 
+        initialRouteName="HomeScreen"
+      >
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            transitionSpec: {
+              open: config,
+              close: config,
+            },
+          }}
+        />
+        <Stack.Screen 
+          name="Resultado" 
+          component={Resultado} 
+          options={{
+            transitionSpec: {
+              open: config,
+              close: config,
+            },
+          }}  
+        />
+        <Stack.Screen 
+          name="Opcoes" 
+          component={Opcoes} 
+          options={{
+            transitionSpec: {
+              open: config,
+              close: config,
+            },
+          }}  
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
